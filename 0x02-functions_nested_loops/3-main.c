3ecfdc264a6222b6beea84e727661e6ca30154f2
Paulette Wacheke <paulettekinguri@gmail.com>
2023-07-19 18:33:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code.
  *
- * Return: Always 0.
+ * Return: 0 always
  */
 int main(void)
 {
@@ -13,8 +12,9 @@ int main(void)
 	_putchar(r + '0');
 	r = _islower('o');
 	_putchar(r + '0');
-	r = _islower(108);
+	r = _islower('1');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
+
 }
