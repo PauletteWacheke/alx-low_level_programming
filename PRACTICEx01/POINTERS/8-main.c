a2dcf2f424dc0c687494c2a6b7b38b7e0d40e0e6
Paulette Wacheke <paulettekinguri@gmail.com>
2023-07-24 10:24:03 -0700

    parameters are passed by value exercise

---
+++ PRACTICEx01/POINTERS/8-main.c
@@ -0,0 +1,27 @@
+/**
+ * modif_my_char_var  - solve me
+ *
+ * Return: nothing
+ */
+
+void modif_my_char_var(char *cc, char ccc)
+{
+	*cc = 'o';
+	ccc = 'l';
+
+}
+
+/**
+ * main - solve me
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+	char *p;
+
+	p = &c;
+	c = 'H';
+	printf("
