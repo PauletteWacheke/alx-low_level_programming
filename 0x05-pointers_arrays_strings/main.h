694fdd25d7a4b347bb654e8d7ba9ed59fc319119
Paulette Wacheke <paulettekinguri@gmail.com>
2023-07-24 13:59:54 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: main.h
- * Auth: Paulette Wacheke
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -22,5 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
