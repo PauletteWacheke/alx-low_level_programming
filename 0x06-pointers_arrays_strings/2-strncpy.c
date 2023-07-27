be918779988b420f4e7b1fea148eefa8371d3c81
Paulette Wacheke <paulettekinguri@gmail.com>
2023-07-27 15:42:45 -0700

    _strncpy - copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,6 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-
+	for (i = 0; i < 0 && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
