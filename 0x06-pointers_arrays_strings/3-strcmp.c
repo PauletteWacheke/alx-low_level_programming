75f38e50d3f3e5591611122b96064020bcd14414
Paulette Wacheke <paulettekinguri@gmail.com>
2023-07-27 16:41:31 -0700

    _strncmp - compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] == s2[i])
 	{
-		if (s1[i] == '\0' || s2[i] == '\0')
-			break;
+		if (s1[i] == '\0')
+			return (0);
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);
-	else
-		return (-1);
+	return (s1[i] - s2[i]);
 }
