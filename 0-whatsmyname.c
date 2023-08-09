7760be3c1af3a324d574363c46e02f27600ab31f
Paulette Wacheke <paulettekinguri@gmail.com>
2023-08-09 19:15:19 +0000

    Program that prints its name followed by a new line

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - program that prints its name, followed by a new line.
+* @argc: argument count
+* @argv: argument vector(array)
+*
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 C PROGRAMMING
+0x0A. C - argc, argv
